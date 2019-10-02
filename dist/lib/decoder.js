af27f3709a366c3044eef667561d2f7471d2020d
Shane Tomlinson <stomlinson@mozilla.com>
2019-10-02 13:55:00 +0100

    Huge decoder speedup by using array.push rather than copying members to a new array.

--- dist/lib/decoder.js
+++ dist/lib/decoder.js
@@ -12,12 +12,13 @@ class FrontDecoder {
                 throw new Error(`Invalid line: ${current}`);
             }
             if (numShared === 0) {
-                return [...result, suffix];
+                result.push(suffix);
             }
             else {
                 const prev = result[index - 1];
-                return [...result, `${prev.slice(0, numShared)}${suffix}`];
+                result.push(`${prev.slice(0, numShared)}${suffix}`);
             }
+            return result;
         }, []);
     }
 }
